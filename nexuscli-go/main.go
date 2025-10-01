0ec46ce1edc005d3d6a1e7e78b57044650c8f655
tympanix <tympanix@gmail.com>
2025-10-01 17:47:56 +0200

    refactor: use cobra for golang cli

--- nexuscli-go/main.go
+++ nexuscli-go/main.go
@@ -1,40 +1,45 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: nexuscli-go <upload|download> [options]")
-		os.Exit(1)
+	var rootCmd = &cobra.Command{
+		Use:   "nexuscli-go",
+		Short: "Nexus CLI for upload and download",
 	}
 
-	switch os.Args[1] {
-	case "upload":
-		uploadCmd := flag.NewFlagSet("upload", flag.ExitOnError)
-		src := uploadCmd.String("src", "", "Directory to upload")
-		dest := uploadCmd.String("dest", "", "Destination in the form 'repository/subdir' (subdir optional)")
-		uploadCmd.Parse(os.Args[2:])
-		if *src == "" || *dest == "" {
-			fmt.Println("upload requires --src and --dest")
-			os.Exit(1)
-		}
-		uploadMain(*src, *dest)
-	case "download":
-		downloadCmd := flag.NewFlagSet("download", flag.ExitOnError)
-		src := downloadCmd.String("src", "", "Nexus RAW folder to download (e.g. 'myrepo/folder' or 'myrepo/folder/subfolder')")
-		dest := downloadCmd.String("dest", "", "Destination directory to save files")
-		downloadCmd.Parse(os.Args[2:])
-		if *src == "" || *dest == "" {
-			fmt.Println("download requires --src and --dest")
-			os.Exit(1)
-		}
-		downloadMain(*src, *dest)
-	default:
-		fmt.Println("Unknown command:", os.Args[1])
+	var uploadCmd = &cobra.Command{
+		Use:   "upload <src> <dest>",
+		Short: "Upload a directory to Nexus RAW",
+		Args:  cobra.ExactArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			src := args[0]
+			dest := args[1]
+			uploadMain(src, dest)
+		},
+	}
+
+	var downloadCmd = &cobra.Command{
+		Use:   "download <src> <dest>",
+		Short: "Download a folder from Nexus RAW",
+		Args:  cobra.ExactArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			src := args[0]
+			dest := args[1]
+			downloadMain(src, dest)
+		},
+	}
+
+	rootCmd.AddCommand(uploadCmd)
+	rootCmd.AddCommand(downloadCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
