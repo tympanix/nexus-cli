79f84fc9a217bd5f1b8f2cfdb412e834cf09b382
tympanix <tympanix@gmail.com>
2025-10-01 17:38:38 +0200

    feat: progress bar for download, golang

--- nexuscli-go/nexus_download.go
+++ nexuscli-go/nexus_download.go
@@ -9,6 +9,8 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
+
+	"github.com/schollz/progressbar/v3"
 )
 
 type Asset struct {
@@ -102,11 +104,40 @@ func downloadFolder(srcArg, destDir string) bool {
 		fmt.Printf("No assets found in folder '%s' in repository '%s'\n", src, repository)
 		return true
 	}
+	// Calculate total bytes to download
+	totalBytes := int64(0)
+	assetSizes := make([]int64, len(assets))
+	for i, asset := range assets {
+		resp, err := http.NewRequest("HEAD", asset.DownloadURL, nil)
+		if err != nil {
+			fmt.Println("Error preparing HEAD request:", err)
+			return false
+		}
+		resp.SetBasicAuth(username, password)
+		res, err := http.DefaultClient.Do(resp)
+		if err != nil {
+			fmt.Println("Error getting asset size:", err)
+			return false
+		}
+		if res.StatusCode != 200 {
+			fmt.Printf("Failed to get size for %s: %d\n", asset.Path, res.StatusCode)
+			res.Body.Close()
+			return false
+		}
+		size := res.ContentLength
+		assetSizes[i] = size
+		totalBytes += size
+		res.Body.Close()
+	}
+	bar := progressbar.DefaultBytes(totalBytes, "Downloading bytes")
+
 	var wg sync.WaitGroup
 	errCh := make(chan error, len(assets))
-	for _, asset := range assets {
+	for i, asset := range assets {
 		wg.Add(1)
-		go downloadAsset(asset, destDir, &wg, errCh)
+		go func(asset Asset, size int64) {
+			downloadAssetWithBar(asset, destDir, &wg, errCh, bar)
+		}(asset, assetSizes[i])
 	}
 	wg.Wait()
 	close(errCh)
@@ -121,6 +152,40 @@ func downloadFolder(srcArg, destDir string) bool {
 		fmt.Printf("Downloaded %d of %d files from '%s' in repository '%s' to '%s'. %d failed.\n", len(assets)-nErrors, len(assets), src, repository, destDir, nErrors)
 	}
 	return nErrors == 0
+}
+
+func downloadAssetWithBar(asset Asset, destDir string, wg *sync.WaitGroup, errCh chan error, bar *progressbar.ProgressBar) {
+	defer wg.Done()
+	path := strings.TrimLeft(asset.Path, "/")
+	localPath := filepath.Join(destDir, path)
+	os.MkdirAll(filepath.Dir(localPath), 0755)
+	resp, err := http.NewRequest("GET", asset.DownloadURL, nil)
+	if err != nil {
+		errCh <- err
+		return
+	}
+	resp.SetBasicAuth(username, password)
+	res, err := http.DefaultClient.Do(resp)
+	if err != nil {
+		errCh <- err
+		return
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		errCh <- fmt.Errorf("Failed to download %s: %d", asset.Path, res.StatusCode)
+		return
+	}
+	f, err := os.Create(localPath)
+	if err != nil {
+		errCh <- err
+		return
+	}
+	defer f.Close()
+	reader := io.TeeReader(res.Body, bar)
+	_, err = io.Copy(f, reader)
+	if err != nil {
+		errCh <- err
+	}
 }
 
 func downloadMain(src, dest string) {
